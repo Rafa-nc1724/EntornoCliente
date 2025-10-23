2a8896c732d2e2b9161ff17c729161d428b0dc2e
Rafa-nc1724 <rniecam350@iesmarquesdecomares.org>
2025-10-23 14:11:26 +0200

    hoal

--- prueba1.js
+++ prueba1.js
@@ -1,4 +1,9 @@
-class Pelicula {
-    
-}
+const misParrafos = [
+                "<p>Este es el primer párrafo.</p>",
+                "<p>Este es el segundo párrafo.</p>",
+                "<p>Este es el tercer párrafo.</p>",
+                "<p>Este es el cuarto párrafo.</p>",
+                "<p>Este es el quinto párrafo.</p>"
+            ];
 
+            
