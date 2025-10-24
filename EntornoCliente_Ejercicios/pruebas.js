9c7a7b1653194c8e05397aaa803a3d26b788377f
Rafa-nc1724 <rniecam350@iesmarquesdecomares.org>
2025-10-24 14:03:05 +0200

    ejercicios hasta el 8

--- EntornoCliente_Ejercicios/pruebas.js
+++ EntornoCliente_Ejercicios/pruebas.js
@@ -1,9 +1,4 @@
-const misParrafos = [
-                "<p>Este es el primer párrafo.</p>",
-                "<p>Este es el segundo párrafo.</p>",
-                "<p>Este es el tercer párrafo.</p>",
-                "<p>Este es el cuarto párrafo.</p>",
-                "<p>Este es el quinto párrafo.</p>"
-            ];
-
-            
+const cambiarEnlace=()=>{
+            let enlace=document.getElementById('enlace');
+            enlace.href=prompt("introduce la nueva dirección web")
+        };
